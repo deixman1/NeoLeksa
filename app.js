beea8c8a5e6a61b70ad174a89b1db965b27a294e
sugar <deix@yandex.ru>
2021-07-23 23:01:35 +0300

    правки

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const usersElement = document.querySelector('#chat');
-const usersAlreadyWrote = ['neoleksa'];
+const usersAlreadyWrote = ['neoleksa', 'ebanuyplayer', 'roma2006qaz'];
+const chat = [
+  {
+    username: 'test',
+    message: 'message',
+    color: '#FF0000'
+  }
+];
 
 const params = new URLSearchParams(window.location.search);
 const channel = params.get('channel') || 'neoleksa';
@@ -14,6 +21,30 @@ const client = new tmi.Client({
   },
   channels: [channel],
 });
+let defaultColors = [
+      '#FF0000','#0000FF','#008000','#B22222','#FF7F50',
+      '#9ACD32','#FF4500','#2E8B57','#DAA520','#D2691E',
+      '#5F9EA0','#1E90FF','#FF69B4','#8A2BE2','#00FF7F'
+    ],
+    randomColorsChosen = {};
+
+function resolveColor(channel, username, color) {
+  if(color !== null) {
+    return color;
+  }
+  if(!(channel in randomColorsChosen)) {
+    randomColorsChosen[channel] = {};
+  }
+  if(username in randomColorsChosen[channel]) {
+    color = randomColorsChosen[channel][username];
+  }
+  else {
+    color = defaultColors[Math.floor(Math.random()*defaultColors.length)];
+    randomColorsChosen[channel][username] = color;
+  }
+  return color;
+}
+
 
 client.connect().then(() => {});
 
@@ -22,29 +53,19 @@ client.on('chat', (channel, userObject, message, self) => {
   console.log(userObject);
   console.log(channel);
   console.log(self);
-  const { username } = userObject;
-  if (username === channel) return;
-    usersElement.innerHTML = usersElement.innerHTML + '<div><h3>' + username + ':</h3><p>' + message + '</p></div>';
-
-  // if (username.toLowerCase() === channel.toLowerCase()) {
-  //   if (message === '!start-count') {
-  //     listeningForCount = true;
-  //   } else if (message === '!end-count') {
-  //     listeningForCount = false;
-  //     // say count out loud.
-  //     const sayCount = new SpeechSynthesisUtterance(Object.keys(users).length);
-  //     window.speechSynthesis.speak(sayCount);
-  //   } else if (message === '!clear-count') {
-  //     countElement.textContent = 'Waiting for count...';
-  //     usersElement.textContent = '';
-  //     users = {};
-  //   }
-  // } else if (listeningForCount && message === '1') {
-  //   users[tags.username] = true;
-  //   // display current count page.
-  //   countElement.textContent = Object.keys(users).length;
-  //   usersElement.textContent = Object.keys(users).join(', ');
-  // }
+  const { username, color } = userObject;
+  if (username === 'neoleksa') return;
+  if (chat.length === 7) chat.shift();
+  chat.push({
+    username: username,
+    message: message,
+    color: color
+  });
+  usersElement.innerHTML = '';
+  for (const user of chat) {
+    usersElement.innerHTML = usersElement.innerHTML +
+        '<div><h3 style="color: ' + user.color + '">' + user.username + ':</h3><p>' + user.message + '</p></div>';
+  }
 });
 
 client.on("join", (channel, username, self) => {
@@ -55,10 +76,12 @@ client.on("join", (channel, username, self) => {
     usersAlreadyWrote.push(username);
   }
 });
+
 client.on("mods", (channel, mods) => {
   console.log('mods');
   console.log(mods);
 });
+
 client.on("subscription", (channel, username, method, message, userstate) => {
   client.say(channel, username + ' подписался на на канал!');
   console.log('subscription' + username);
